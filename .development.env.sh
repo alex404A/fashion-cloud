a75bd7e5fbe6019e27f2b0cf32db48c183391c93
alex404A <maxmalex1990@gmail.com>
2020-05-02 23:35:38 +0800

    enhance: remove env configuration

--- .development.env.sh
+++ .development.env.sh
@@ -2,10 +2,10 @@
 
 export ENV="development"
 export APPLICATION_NODE_PORT="8999"
-export MONGODB_USERNAME="next"
+export MONGODB_USERNAME="alex"
 export MONGODB_PASSWORD="common!dev"
-export MONGODB_HOST="128.106.66.190"
-export MONGODB_PORT="57017"
+export MONGODB_HOST="127.0.0.1"
+export MONGODB_PORT="27017"
 export MONGODB_DB="trial"
 export MONGODB_AUTHDB="admin"
 export CACHE_TTL="600000"
--- .test.env.sh
+++ .test.env.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-export APPLICATION_NODE_PORT="8999"
 export ENV="test"
-export MONGODB_USERNAME="next"
+export APPLICATION_NODE_PORT="8999"
+export MONGODB_USERNAME="alex"
 export MONGODB_PASSWORD="common!dev"
-export MONGODB_HOST="128.106.66.190"
-export MONGODB_PORT="57017"
-export MONGODB_DB="trial_test"
+export MONGODB_HOST="127.0.0.1"
+export MONGODB_PORT="27017"
+export MONGODB_DB="trial"
 export MONGODB_AUTHDB="admin"
-export CACHE_TTL="10000"
+export CACHE_TTL="600000"
 export CACHE_SCALE="2"
